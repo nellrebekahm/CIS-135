e6860b756686b6056b77a40af8163641f0f29af4
nellrebekahm <96350340+nellrebekahm@users.noreply.github.com>
2021-12-18 20:11:52 -0600

    Add files via upload

--- nell_final.py
+++ nell_final.py
@@ -2,13 +2,110 @@
 # CIS 135
 # nell_final.py
 
-from student import all_students
+# imported data from student.py
+import student
 
 
+# function to display and navigate main menu
+def print_main_menu():
+    print('\n\t\t Main Menu \n1) Print a List of All Students \n2) Add a New Student \n3) Manage a Student Record '
+          '\n4) Exit the '
+          'Program')
+
+
+# main menu option 1: prints the student directory
+def print_all_students(all_students):
+    print('\n\t\t Student Directory')
+    for each in all_students:
+        each.print_student()
+    return
+
+
+# main menu option 2: prompts the user to enter student information and creates a new entry in the directory if
+# student doesn't already exist
+def create_new_student(students):
+    print('\nAdd a New Student')
+    get_first_name = input('Please enter the student\'s first name: ')
+    get_last_name = input('Please enter the student\'s last name: ')
+    get_student_id = input('Please enter the student\'s number: ')
+    if select_student_by_id(students, get_student_id) is None:
+        new_student = student.Student(get_first_name, get_last_name, get_student_id)
+        students.append(new_student)
+    else:
+        print("Student Exists Already")
+
+    return students
+
+
+# main menu option 3: displays a menu which allows the user to further edit previous student entries
+def print_student_menu():
+    print('\n\t\t Student Menu \n1) Add a Class to a Student Record \n2) Print an Individual Student Record \n3) '
+          'Exit '
+          'Student Menu')
+
+
+# student menu option 1: asks the user to enter the student ID which they would like to edit, then pulls up the
+# student associated with that ID and allows the user to add classes taken and grades received
+def add_class_for_student(classes):
+    student_ID_search = input('Please enter the ID of the student you wish to search: ')
+    if select_student_by_id(classes, student_ID_search) is not None:
+        get_class_name = input('\nEnter class name: ')
+        get_class_grade = input('Enter letter grade received: ')
+        new_class = student.Class(get_class_name, get_class_grade)
+        classes.append(new_class)
+        return
+    else:
+        print('Student not found. Please try again.')
+    return classes
+
+
+# student menu option 2: prompts the user to enter the student ID which they would like to view, then displays all of
+# the information that the user has entered for the student
+def print_student_record(classes_completed):
+    print('\n\t\t Student Classes')
+    for each in classes_completed:
+        each.add_class()
+    return
+
+
+# this function allows the user to search for a specific student and their information by entering their ID number
+def select_student_by_id(students, studentID):
+    for student in students:
+        if studentID == student.student_id:
+            return student
+
+    return None
+
+
+# main program function
 def main():
+    all_students = []
+    classes_completed = []
     main_menu_selection = 0
     student_menu_selection = 0
-    # all_students = []
 
-    while main_menu_selection != "Exit":
-        print_main_menu
+    while main_menu_selection != '4':
+        print_main_menu()
+        selection = input('\nPlease select a menu option: ')
+        if selection == '1':
+            print_all_students(all_students)
+        elif selection == '2':
+            all_students = create_new_student(all_students)
+        elif selection == '3':
+            while student_menu_selection != '3':
+                print_student_menu()
+                selection = input('\nPlease select a menu option: ')
+                if selection == '1':
+                    add_class_for_student(all_students)
+                elif selection == '2':
+                    print_student_record(classes_completed)
+                elif selection == '3':
+                    print_main_menu()
+                    break
+        elif selection == '4':
+            exit()
+        else:
+            print('\nThat is not a valid selection, please try again.')
+
+
+main()
--- student.py
+++ student.py
@@ -3,49 +3,42 @@
 # nell_final.py
 
 
+# Student class which stores the student information enter by the user
 class Student:
     def __init__(self, first_name, last_name, student_id):
         self.first_name = first_name
         self.last_name = last_name
         self.student_id = student_id
-        self.classes_completed = {}
-
         return
 
     def print_student(self):
         print(f'{self.first_name.title()} {self.last_name.title()} {self.student_id.upper():>20}')
         return
 
-    def add_class(self):
-        class_name = input('Enter class name: ')
-        grade = input('Enter letter grade received: ')
+# Class class which stores student class information entered by the user
+class Class:
+    def __init__(self, class_name, class_grade):
+        self.class_name = class_name
+        self.class_grade = class_grade
 
+    def add_class(self):
+        print(f'{self.class_name.upper()} {self.class_grade.upper()}')
         return
 
-    # def compute_gpa(self):
-    #     grade_points = {'A': 4, 'B': 3, 'C': 2, 'D': 1, 'F': 0}
+# function to calculate student GPA
+def compute_gpa():
+    get_GPA = 0
+    # if get_class_grade == 'A':
+    #     get_GPA += 4
+    # elif get_class_grade == 'B':
+    #     get_GPA += 3
+    # elif get_class_grade == 'C':
+    #     get_GPA += 2
+    # elif get_class_grade == 'D':
+    #     get_GPA += 1
+    # elif get_class_grade == 'F':
+    #     get_GPA += 0
+    # else:
+    #     'This letter grade does not exist, please try again.'
 
-
-
-
-# class Grade:
-#     def __init__(self, class_name, grade):
-#         self.class_name = class_name
-#         self.grade = grade
-#         return
-#
-#     def classes_completed(self):
-#         print(f'{self.class_name.title()} {self.grade}')
-#         return
-#
-
-
-
-#
-# fn = 'rebecca'
-# ln = 'nell'
-# s_id = '123456'
-# new_student = Student(fn, ln, s_id)
-# print(type(new_student))
-# new_student.print_student()
 
